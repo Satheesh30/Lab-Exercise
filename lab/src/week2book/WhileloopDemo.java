42a5ad72e860d8f43178f8dd980528fa45bd3e0b
satheesh kumar p <psk30198@gmail.com>
2021-06-15 22:32:35 +0530

    fisrt commit

---
+++ lab/src/week2book/WhileloopDemo.java
@@ -0,0 +1,15 @@
+package week2book;
+
+public class WhileloopDemo {
+	public static void main(String[] args) {
+
+ int n=1,sum=0;
+while(n<=10) {
+	sum=sum+n;
+	n++;
+	
+}
+System.out.print("summation : "+sum);
+}
+}
+
